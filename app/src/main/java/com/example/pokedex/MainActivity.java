7480e2db1d1bb3cd5cd20ce9dc5d3a55709d9add
Hu3diger <martinruediger2010@gmail.com>
2019-04-16 20:42:45 -0300

    finished²

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -1,36 +1,53 @@
 package com.example.pokedex;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
+
 public class MainActivity extends AppCompatActivity {
-    private String responseJSON;
-    private Button btn;
-    private ImageView imgView;
-    private TextView txtView;
-    private static final String TAG = "MainActivity";
+    ImageView img;
+    TextView roundImg, acertos, erros;
+
+    //Options buttons
+    Button opt1;
+    Button opt2;
+    Button opt3;
+    Button opt4;
+
+    //Json Array, like a pokedex
+    JSONArray pokedex;
+    int right, rightCount, round, ers;
+    JSONObject rightPokemon;
+    JSONObject pokemon;
+    String imgUrl, rightName;
+    ArrayList<String> optionsNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,90 +57,142 @@ public class MainActivity extends AppCompatActivity {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-
-        btn = findViewById(R.id.button);
-        imgView = findViewById(R.id.imageView2);
-        txtView = findViewById(R.id.textView);
-        btn.setOnClickListener(new View.OnClickListener() {
+        roundImg = findViewById(R.id.rodada);
+        acertos = findViewById(R.id.acertosCount);
+        erros = findViewById(R.id.errosCount);
+        img = findViewById(R.id.pokemonImg);
+        opt1 = findViewById(R.id.opt1);
+        opt2 = findViewById(R.id.opt2);
+        opt3 = findViewById(R.id.opt3);
+        opt4 = findViewById(R.id.opt4);
+        Pokedex object = new Pokedex();
+        object.downloadJSON();
+        pokedex = object.loadJSONobject();
+        rightCount = 0;
+        round = 1;
+        ers = 0;
+        roundImg.setText(String.valueOf(round) + "/30");
+        newGame();
+        opt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                downloadJSON();
+                checkIfRight(opt1.getText().toString());
+            }
+        });
+        opt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkIfRight(opt2.getText().toString());
+            }
+        });
+        opt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkIfRight(opt3.getText().toString());
+            }
+        });
+        opt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkIfRight(opt4.getText().toString());
             }
         });
     }
 
-
-    public void downloadJSON() {
-        URL url;
-        StringBuffer response = new StringBuffer();
+    public void newGame() {
         try {
-            url = new URL("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
-        } catch (MalformedURLException e) {
-            throw new IllegalArgumentException("invalid url");
-        }
+            Random rnd = new Random();
 
-        HttpURLConnection conn = null;
-        try {
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(false);
-            conn.setDoInput(true);
-            conn.setUseCaches(false);
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+            optionsNames = new ArrayList<>();
 
-            // handle the response
-            int status = conn.getResponseCode();
-            if (status != 200) {
-                throw new IOException("Post failed with error code " + status);
-            } else {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-            }
+            //Set the button with the right pokemon
+            right = rnd.nextInt(4);
+
+            //Get the right pokemon from JSON
+            rightPokemon = pokedex.getJSONObject(rnd.nextInt(151));
+            imgUrl = rightPokemon.getString("img");
+            downloadImage(imgUrl);
+            rightName = rightPokemon.getString("name");
+
+            //Initialize buttons list
+            optionsNames.add("");
+            optionsNames.add("");
+            optionsNames.add("");
+            optionsNames.add("");
+            acertos.setText(String.valueOf(rightCount));
+            erros.setText(String.valueOf(ers));
+            roundImg.setText(String.valueOf(round) + "/30");
+            round++;
+            setButtons();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-
-            //Here is your json in string format
-            responseJSON = response.toString();
-            loadJSONobject();
         }
     }
 
-    private void loadJSONobject() {
-        String data = "";
+    public void checkIfRight(String option) {
+        if (option.equals(rightName)) {
+            rightCount++;
+            Context context = getApplicationContext();
+            CharSequence text = "Você acertou!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "Você errou, o nome era " + rightName;
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            ers++;
+        }
+        if (round == 30) {
+            this.openDialog(rightCount);
+        } else {
+            newGame();
+        }
+    }
+
+    public void openDialog(int n) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Você acertou " + n + " de 30 questões")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        round = 1;
+                        rightCount = 0;
+                        ers = 0;
+                        newGame();
+                        dialog.dismiss();
+                    }
+                });
+        builder.show();
+    }
+
+    public void setButtons() {
         try {
-            JSONObject jsonRootObject = new JSONObject(responseJSON);
-
-            JSONArray jsonArray = jsonRootObject.optJSONArray("pokemon");
-
-            Random random = new Random();
-            int index = random.nextInt(150)+1;
-
-            JSONObject jsonObject = jsonArray.getJSONObject(index);
-            int id = Integer.parseInt(jsonObject.optString("id").toString());
-            String name = jsonObject.optString("name").toString();
-            String img = jsonObject.optString("img").toString();
-            downloadImage(img);
-            txtView.setText("id: " + id + " - nome: " + name);
-        } catch (JSONException e) {
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++) {
+                pokemon = pokedex.getJSONObject(rnd.nextInt(151));
+                if (i == right) {
+                    optionsNames.add(i, rightName);
+                } else {
+                    optionsNames.add(i, pokemon.getString("name"));
+                }
+            }
+            opt1.setText(optionsNames.get(0));
+            opt2.setText(optionsNames.get(1));
+            opt3.setText(optionsNames.get(2));
+            opt4.setText(optionsNames.get(3));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 
     public void downloadImage(String imageUrl) {
         ImageDownloader imageDownloader = new ImageDownloader();
         imageUrl = imageUrl.replace("http", "https");
         try {
             Bitmap imagem = imageDownloader.execute(imageUrl).get();
-            imgView.setImageBitmap(imagem);
+            img.setImageBitmap(imagem);
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
